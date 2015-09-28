cf29d87d004729a3df7f42f3371d304069e7d527
Josh Chernoff <digitalcakestudios@gmail.com>
2015-09-27 18:37:39 -0700

    Add organizer field

--- cloud/routes.js
+++ cloud/routes.js
@@ -1,49 +1,50 @@
 module.exports = function(app) {
 
-	var path				   = require('path');
-	var parse 				 = require('parse').Parse;
+  var path           = require('path');
+  var parse          = require('parse').Parse;
 
-	app.get('/public/states.js', function(req, res){
-		res.sendFile('states.js', { root: path.join(__dirname, '../public') });	
-	});
+  app.get('/public/states.js', function(req, res){
+    res.sendFile('states.js', { root: path.join(__dirname, '../public') });
+  });
 
-	app.get('/public/app.js', function(req, res){
-		res.sendFile('app.js', { root: path.join(__dirname, '../public') });
-	});
+  app.get('/public/app.js', function(req, res){
+    res.sendFile('app.js', { root: path.join(__dirname, '../public') });
+  });
 
-	app.get('/', function(req, res){
-	  res.sendFile('index.html', { root: path.join(__dirname, '../public') });
-	});
+  app.get('/', function(req, res){
+    res.sendFile('index.html', { root: path.join(__dirname, '../public') });
+  });
 
-	app.post('/api/event', function(res, req){
-		
+  app.post('/api/event', function(res, req){
     parse.initialize(process.env.applicationId, process.env.javascriptId);
 
-		var e = req.req.body;
-		var Events = parse.Object.extend('Event');
-		var event = new Events();
+    var e = req.req.body;
+    console.log(e);
+    var Events = parse.Object.extend('Event');
+    var event = new Events();
 
-		event.set('eventName', e.name);
-		event.set('eventStart', e.startDate.length > 0 ? new Date(e.startDate) : undefined);
-		event.set('eventEnd', e.endDate.length > 0 ? new Date(e.endDate) : undefined);
-		event.set('eventCategory', e.category);
-		event.set('eventDescription', e.description);
-		event.set('website', e.website);
-		event.set('locationName', e.location);
-		event.set('addressOne', e.addressOne);
-		event.set('addressTwo', e.addressTwo);
-	  	event.set('city', e.city);
-	  	event.set('locationState', e.state.abbreviation);
-	  	event.set('zip', e.zip);
-		event.set('price', e.price.length > 0 && isFinite(e.price) ? parseInt(e.price) : 0);
+    event.set('eventName', e.name);
+    event.set('eventStart', e.startDate.length > 0 ? new Date(e.startDate) : undefined);
+    event.set('eventEnd', e.endDate.length > 0 ? new Date(e.endDate) : undefined);
+    event.set('eventCategory', e.category);
+    event.set('eventDescription', e.description);
+    event.set('website', e.website);
+    event.set('organizer', e.organizer);
+    event.set('locationName', e.location);
+    event.set('addressOne', e.addressOne);
+    event.set('addressTwo', e.addressTwo);
+    event.set('city', e.city);
+    event.set('locationState', e.state.abbreviation);
+    event.set('zip', e.zip);
+    event.set('price', e.price.length > 0 && isFinite(e.price) ? parseInt(e.price) : 0);
 
 
-		event.save().then(
-			function(data) {
-				res.res.json({success: true});
-			}, 
-			function(error){
-				res.res.json({error: error});
-			});
-	});
+    event.save().then(
+      function(data) {
+        res.res.json({success: e});
+      },
+      function(error){
+        res.res.json({error: error});
+      });
+  });
 };
--- public/app.js
+++ public/app.js
@@ -1,64 +1,65 @@
 angular.module('events', [
-	'ngRoute',
-	'ui.bootstrap'
+  'ngRoute',
+  'ui.bootstrap'
 ])
 
 .controller('homeCtrl', ['$scope', '$http', function($scope, $http){
-	$scope.states = usStates;
-	
-	$scope.init = function(){
-		$scope.event = {
-			name: '',
-			startDate: '',
-			endDate: '',
-			category: '',
-			description: '',
-			website: '',
-			location: '',
-			addressOne: '',
-			addressTwo: '',
-			city: '',
-			state: '',
-			zip: '',			
-			price: 0
-		}
-	};
+  $scope.states = usStates;
 
-	$scope.init();
+  $scope.init = function(){
+    $scope.event = {
+      name: '',
+      startDate: '',
+      endDate: '',
+      category: '',
+      description: '',
+      website: '',
+      organizer: '',
+      location: '',
+      addressOne: '',
+      addressTwo: '',
+      city: '',
+      state: '',
+      zip: '',
+      price: 0
+    }
+  };
 
-	$scope.open = function($event, opened) {
-		$event.preventDefault();
-	    $event.stopPropagation();
+  $scope.init();
 
-	    $scope[opened] = true;
-  	};
+  $scope.open = function($event, opened) {
+    $event.preventDefault();
+    $event.stopPropagation();
 
-  	$scope.enterEvent = function() {
-  		$scope.msg = '';
-  		var e = $scope.event;
+    $scope[opened] = true;
+  };
 
-		$http({method: 'POST', url: 'api/event', data: e })
-			.success(function(data, status){
-				if(data.success) {
-					window.scrollTo(0,0);
-					$scope.init();
-				}
-			})
-			.error(function(data, status){
-				console.log(data);
-			});
-  	};
+  $scope.enterEvent = function() {
+    $scope.msg = '';
+    var e = $scope.event;
 
-  	$scope.checkNumber = function(evt) {
-  		var theEvent = evt || window.event;
-		var key = theEvent.keyCode || theEvent.which;
-		key = String.fromCharCode( key );
-		var regex = /[0-9]|\./;
-		if( !regex.test(key) ) {
-		theEvent.returnValue = false;
-		if(theEvent.preventDefault) theEvent.preventDefault();
-		}
-  	}
+    $http({method: 'POST', url: 'api/event', data: e })
+    .success(function(data, status){
+      if(data.success) {
+        window.scrollTo(0,0);
+        $scope.init();
+      }
+    })
+    .error(function(data, status){
+      console.log(data);
+    });
+  };
+
+  $scope.checkNumber = function(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode( key );
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
 
 }]);
 
