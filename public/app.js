7c8fd74e3f6639a33f5feefc2caf2f41191e1b65
dswiese <sean@wiese.us>
2015-10-04 11:52:29 -0700

    Revert "Update description and add organizer"

--- public/app.js
+++ public/app.js
@@ -1,67 +1,64 @@
 angular.module('events', [
-  'ngRoute',
-  'ui.bootstrap'
+	'ngRoute',
+	'ui.bootstrap'
 ])
 
 .controller('homeCtrl', ['$scope', '$http', function($scope, $http){
-  $scope.states = usStates;
+	$scope.states = usStates;
+	
+	$scope.init = function(){
+		$scope.event = {
+			name: '',
+			startDate: '',
+			endDate: '',
+			category: '',
+			description: '',
+			website: '',
+			location: '',
+			addressOne: '',
+			addressTwo: '',
+			city: '',
+			state: '',
+			zip: '',			
+			price: 0
+		}
+	};
 
-  $scope.init = function(){
-    $scope.event = {
-      name: '',
-      startDate: '',
-      endDate: '',
-      category: '',
-      description: '',
-      website: '',
-      organizer: '',
-      location: '',
-      addressOne: '',
-      addressTwo: '',
-      city: '',
-      state: '',
-      zip: '',
-      price: 0
-    }
-    tinymce.init({selector:'textarea', menubar: false, height: 300 });
-  };
+	$scope.init();
 
-  $scope.init();
+	$scope.open = function($event, opened) {
+		$event.preventDefault();
+	    $event.stopPropagation();
 
-  $scope.open = function($event, opened) {
-    $event.preventDefault();
-    $event.stopPropagation();
+	    $scope[opened] = true;
+  	};
 
-    $scope[opened] = true;
-  };
+  	$scope.enterEvent = function() {
+  		$scope.msg = '';
+  		var e = $scope.event;
 
-  $scope.enterEvent = function() {
-    $scope.msg = '';
-    var e = $scope.event;
-    e.description = tinyMCE.activeEditor.getContent();
-    $http({method: 'POST', url: 'api/event', data: e })
-    .success(function(data, status){
-      if(data.success) {
-        window.scrollTo(0,0);
-        tinymce.get('tinymce2').setContent('');
-        $scope.init();
-      }
-    })
-    .error(function(data, status){
-      console.log(data);
-    });
-  };
+		$http({method: 'POST', url: 'api/event', data: e })
+			.success(function(data, status){
+				if(data.success) {
+					window.scrollTo(0,0);
+					$scope.init();
+				}
+			})
+			.error(function(data, status){
+				console.log(data);
+			});
+  	};
 
-  $scope.checkNumber = function(evt) {
-    var theEvent = evt || window.event;
-    var key = theEvent.keyCode || theEvent.which;
-    key = String.fromCharCode( key );
-    var regex = /[0-9]|\./;
-    if( !regex.test(key) ) {
-      theEvent.returnValue = false;
-      if(theEvent.preventDefault) theEvent.preventDefault();
-    }
-  }
+  	$scope.checkNumber = function(evt) {
+  		var theEvent = evt || window.event;
+		var key = theEvent.keyCode || theEvent.which;
+		key = String.fromCharCode( key );
+		var regex = /[0-9]|\./;
+		if( !regex.test(key) ) {
+		theEvent.returnValue = false;
+		if(theEvent.preventDefault) theEvent.preventDefault();
+		}
+  	}
 
 }]);
 
